f208828e808a670bf6192b4f5e1c099278e463fe
Thomas Karl <thomas@xciteit.com>
2022-09-18 18:56:41 +0200

    fix: introduced waiting for AWS Lambda function state after function updates
    this change is necessary to be compatible with AWS Lambda functions as of January 31 2022.
    see https://aws.amazon.com/blogs/compute/coming-soon-expansion-of-aws-lambda-states-to-all-functions/ for details.

--- src/aws/Lambda.ts
+++ src/aws/Lambda.ts
@@ -4,6 +4,7 @@ import {
   GetFunctionConfigurationCommandOutput,
   UpdateFunctionCodeCommand,
   PublishVersionCommand,
+  LastUpdateStatus,
 } from '@aws-sdk/client-lambda';
 import { Buffer } from 'buffer';
 import * as fs from 'fs/promises';
@@ -35,7 +36,7 @@ export const deployFunction = async (
 
   return Promise.all([filePromise, checkPromise])
     .then(deployCode(client))
-    .then(publishOrReject(publish))
+    .then(publishOrReject(Publish.Force))
     .then(publishVersion(client, description))
     .catch((err) => {
       if (err === Publish.Never) {
@@ -47,21 +48,52 @@ export const deployFunction = async (
 
 const publishVersion =
   (client: LambdaClient, description: string) =>
-  async (newFunctionData: GetFunctionConfigurationCommandOutput): Promise<string> => {
-    const publishVersionCommand = new PublishVersionCommand({
-      FunctionName: newFunctionData.FunctionName,
-      RevisionId: newFunctionData.RevisionId,
-      Description: description,
-    });
+  async (functionData: GetFunctionConfigurationCommandOutput): Promise<string> => {
+    if (!functionData.FunctionName) {
+      throw 'No function name in function configuration.';
+    }
 
-    return client.send(publishVersionCommand).then((newFunctionData) => {
-      if (!newFunctionData.Version) {
-        throw 'Publish version failed: Could not retrieve new version';
-      }
-      console.log('Function published. Version', newFunctionData.Version);
-      return newFunctionData.Version;
+    return awaitStatusSuccess(client, functionData.FunctionName, 100).then(async (functionData) => {
+      const publishVersionCommand = new PublishVersionCommand({
+        FunctionName: functionData.FunctionName,
+        RevisionId: functionData.RevisionId,
+        Description: description,
+      });
+      return client.send(publishVersionCommand).then((functionData) => {
+        if (!functionData.Version) {
+          throw 'Publish version failed: Could not retrieve new version';
+        }
+        console.log('Function published. Version', functionData.Version);
+        return functionData.Version;
+      });
     });
   };
+
+const awaitStatusSuccess = async (
+  client: LambdaClient,
+  functionName: string,
+  tries: number = 1,
+  interval: number = 5000
+): Promise<GetFunctionConfigurationCommandOutput> => {
+  return getFunctionConfig(client)(functionName).then((response) => {
+    if (tries <= 0) {
+      throw 'Retries exhausted while waiting for status "Success", giving up.';
+    }
+    if (response.LastUpdateStatus === LastUpdateStatus.Failed) {
+      throw 'Encountered status "Failed" while waiting for LastUpdateStatus "Success", terminating.';
+    }
+
+    const status: string = response && response.LastUpdateStatus ? response.LastUpdateStatus : 'Could not read status';
+    if (status === LastUpdateStatus.Successful) {
+      console.log(`Function status: ${status}. Continuing.`);
+      return Promise.resolve(response);
+    }
+    console.log(`Function status: ${status}. Checking again in ${interval}ms. ${tries} tries left.`);
+    return new Promise((resolve) =>
+      setTimeout(() => resolve(awaitStatusSuccess(client, functionName, tries - 1, interval)), interval)
+    );
+  });
+};
 
 const deployCode =
   (client: LambdaClient) =>
@@ -98,23 +130,31 @@ const publishOrReject =
     return Promise.resolve(newFunctionData);
   };
 
-const checkFunction = (client: LambdaClient) => async (functionName: string) => {
-  console.log('Checking if function exists.');
-  const checkFunctionCommand = new GetFunctionConfigurationCommand({
-    FunctionName: functionName,
-  });
-  return client.send(checkFunctionCommand).then((response) => {
-    console.log(
-      'Found function. Version',
-      response.Version,
-      'RevisionId',
-      response.RevisionId,
-      'CodeSha256',
-      response.CodeSha256
-    );
-    return response;
-  });
-};
+const checkFunction =
+  (client: LambdaClient) =>
+  async (functionName: string): Promise<GetFunctionConfigurationCommandOutput> => {
+    console.log('Checking if function exists.');
+    return getFunctionConfig(client)(functionName).then((response) => {
+      console.log(
+        'Found function. Version',
+        response.Version,
+        'RevisionId',
+        response.RevisionId,
+        'CodeSha256',
+        response.CodeSha256
+      );
+      return response;
+    });
+  };
+
+const getFunctionConfig =
+  (client: LambdaClient) =>
+  async (functionName: string): Promise<GetFunctionConfigurationCommandOutput> => {
+    const getFunctionConfigCommand = new GetFunctionConfigurationCommand({
+      FunctionName: functionName,
+    });
+    return client.send(getFunctionConfigCommand);
+  };
 
 const readZipAsBuffer = async (path: string): Promise<Buffer> => {
   console.info('Reading ZIP file...');
